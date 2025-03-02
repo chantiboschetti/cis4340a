84221c219f779cad7d1b29cf7bef3dad8181efd6
chantiboschetti <97052239+chantiboschetti@users.noreply.github.com>
2025-03-01 23:55:11 -0500

    Update R09_LCK02_J.java

--- R09_LCK02_J.java
+++ R09_LCK02_J.java
@@ -1,3 +1,6 @@
+//Rule 09. Locking (LCK)
+//LCK02-J. Do not synchronize on the class object returned by getClass()
+//compliant solution below:
 class Base {
   static DateFormat format =
       DateFormat.getDateInstance(DateFormat.MEDIUM);
