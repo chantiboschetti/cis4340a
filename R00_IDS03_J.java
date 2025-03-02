88260e9e86f5507ac0f2be0797c94851d9cc5627
chantiboschetti <97052239+chantiboschetti@users.noreply.github.com>
2025-03-01 23:44:23 -0500

    Update R00_IDS03_J.java

--- R00_IDS03_J.java
+++ R00_IDS03_J.java
@@ -1,5 +1,6 @@
 //Rule 00. Input Validation and Data Sanitization (IDS)
-//IDS03-J. Do not log unsanitized user input Given the non-compliant code below:
+//IDS03-J. Do not log unsanitized user input
+//Compliant Solution below:
 public String sanitizeUser(String username) {
   return Pattern.matches("[A-Za-z0-9_]+", username))
       ? username : "unauthorized user";
