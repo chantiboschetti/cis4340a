5d57d029832fce2ab3a3c0d0a47e30a7bc959627
chantiboschetti <97052239+chantiboschetti@users.noreply.github.com>
2025-03-01 23:36:05 -0500

    Update R09_LCK02_J.java
    compliant solution for rule 09.

--- R09_LCK02_J.java
+++ R09_LCK02_J.java
@@ -3,17 +3,10 @@ class Base {
       DateFormat.getDateInstance(DateFormat.MEDIUM);
  
   public Date parse(String str) throws ParseException {
-    synchronized (getClass()) {
+    synchronized (Base.class) {
       return format.parse(str);
     }
   }
 }
  
-class Derived extends Base {
-  public Date doSomethingAndParse(String str) throws ParseException {
-    synchronized (Base.class) {
-      // ...
-      return format.parse(str);
-    }
-  }
-}
+// ...
